70edf6d5d1681fcdef4877da3bfcfc9ddaa16f89
Vikas Pal <vikas.pal@myparkplus.com>
2023-11-16 12:49:20 +0530

    mutex and locking changes is added as per the concurrency

--- Scripts/Mutex/main_v2.go
+++ Scripts/Mutex/main_v2.go
@@ -5,6 +5,11 @@ package main
   - Run Two functions : Reader, Writer : there will be issue of dirty read-write functions.
   - Mutex : helps to lock the shared resources.
     a.RwMutex : Multiple readers can reads the data but anyone can access.
+
+Error Handling Major Design Pattern:
+1. Return in the functions as the last value.
+2. Handling error if error != nil then do the stuff.
+3. Wrapping Error : need to some additional information, then will be pull the first place we have added here.
 */
 func main() {
 	return
