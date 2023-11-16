d2ea92310bf493c9a5b6502877b15556d8bb448f
Vikas Pal <vikas.pal@myparkplus.com>
2023-11-16 12:25:13 +0530

    hot fixed

---
+++ Scripts/Mutex/main_v2.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+  - Create a Global Maps
+  - Run Two functions : Reader, Writer : there will be issue of dirty read-write functions.
+  - Mutex : helps to lock the shared resources.
+    a.RwMutex : Multiple readers can reads the data but anyone can access.
+*/
+func main() {
+
+	return
+}
