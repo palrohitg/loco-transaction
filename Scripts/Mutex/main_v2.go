bc1596c64908e49ae406ad82030574879e0ba95d
Vikas Pal <vikas.pal@myparkplus.com>
2023-11-29 22:03:50 +0530

    new text here

--- Scripts/Mutex/main_v2.go
+++ Scripts/Mutex/main_v2.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
   - Create a Global Maps
   - Run Two functions : Reader, Writer : there will be issue of dirty read-write functions.
@@ -11,6 +13,28 @@ Error Handling Major Design Pattern:
 2. Handling error if error != nil then do the stuff.
 3. Wrapping Error : need to some additional information, then will be pull the first place we have added here.
 */
+
+/*
+- Ways to Handle Error Handling
+1. Returning Error from the function calls.
+2. Panic and Cover : Panic is used to stop the program executions.
+3. Errors Package : Also write the UnitTest of the Code in golang : Errors.Is, Errors.NotIn -> OSExists
+*/
 func main() {
-	return
+	fmt.Println("Start of main")
+	doSomething()
+	fmt.Println("End of main")
+	fmt.Println("End of main")
+	fmt.Println("End of main")
+}
+
+func doSomething() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+		}
+	}()
+	fmt.Println("Start of doSomething")
+	panic("something went wrong")
+	fmt.Println("End of doSomething") // This won't be executed due to panic
 }
